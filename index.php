087920d12897a6ea24bbbd933a2af6e638d92a12
EduardoZafraMartin <72192046+EduardoZafraMartin@users.noreply.github.com>
2022-10-13 20:48:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,44 @@
-<?php
-
-// Llamada al controlador
-require_once("controllers/login_controller.php");
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Gaming</title>
+  <link rel="stylesheet" href="css/styleIndex.css">
+</head>
+<body>
+  <div class="container">
+    <div class="card">
+      <div class="box">    
+        <div class="content">
+        <h2>IES</h2>
+        <h3>Turno Diurno</h3>
+        <p>Pincha aquí si perteneces al turno diurno</p>
+        <a href="views/login_view.php">Tengo una cuenta</a>
+      </div> 
+    </div> 
+    </div>
+    <div class="card">
+      <div class="box">    
+        <div class="content">
+        <h2>Leonardo</h2>
+        <h3>Turno Vespertino</h3>
+        <p>Pincha aquí si perteneces al turno vespertino</p>
+        <a href="views/login_view.php">Tengo una cuenta</a>
+      </div> 
+    </div> 
+    </div>
+    <div class="card">
+      <div class="box">    
+        <div class="content">
+        <h2>Da Vinci</h2>
+        <h3>Registrate</h3>
+        <p>Pincha aquí para registrarte y unirte a la comunidad gaming</p>
+        <a href="views/register_view.php">Registrate YA</a>
+      </div> 
+    </div> 
+    </div>
+  </div>  
+</body>
+</html>
