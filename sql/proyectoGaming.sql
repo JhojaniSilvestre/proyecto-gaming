586e0908a6403fa1739d5a15f78f98d08dbf1cca
edu zafra <eduardozaframartin@gmail.com>
2022-10-18 18:48:45 +0200

    cambio conexion base de datos

--- sql/proyectoGaming.sql
+++ sql/proyectoGaming.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE `proyectoGaming`;
-
 CREATE TABLE `users` (
   `id_user` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL,
@@ -20,33 +18,33 @@ CREATE TABLE proyectoGaming.admins (
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci;
-DROP TABLE IF EXISTS proyectoGaming.admins;
 
-CREATE TABLE proyectoGaming.seats (
+
+CREATE TABLE seats (
 	id_seat INT auto_increment NOT NULL,
 	CONSTRAINT seats_pk PRIMARY KEY (id_seat)
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci;
-DROP TABLE IF EXISTS proyectoGaming.seats;
 
-CREATE TABLE proyectoGaming.booking (
+
+CREATE TABLE booking (
 	id_booking INT auto_increment NOT NULL,
 	`date` DATETIME NOT NULL,
 	id_companion INT NULL,
 	id_seat INT NOT NULL,
 	id_user INT NOT NULL,
 	CONSTRAINT booking_pk PRIMARY KEY (id_booking),
-	CONSTRAINT booking_FK FOREIGN KEY (id_user) REFERENCES proyectoGaming.users(id_user) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT booking_FK_1 FOREIGN KEY (id_seat) REFERENCES proyectoGaming.seats(id_seat) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT booking_FK FOREIGN KEY (id_user) REFERENCES users(id_user) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT booking_FK_1 FOREIGN KEY (id_seat) REFERENCES seats(id_seat) ON DELETE CASCADE ON UPDATE CASCADE
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci;
-DROP TABLE IF EXISTS proyectoGaming.booking;
 
-CREATE TABLE proyectoGaming.games (
+
+CREATE TABLE games (
 	id_game INT auto_increment NOT NULL,
 	name varchar(100) NOT NULL,
 	CONSTRAINT games_pk PRIMARY KEY (id_game)
@@ -54,49 +52,49 @@ CREATE TABLE proyectoGaming.games (
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci;
-DROP TABLE IF EXISTS proyectoGaming.games;
 
-CREATE TABLE proyectoGaming.tournaments (
+
+CREATE TABLE tournaments (
 	id_tournament INT auto_increment NOT NULL,
 	name varchar(100) NOT NULL,
 	`date` DATETIME NOT NULL,
 	id_game INT NOT NULL,
 	id_admin INT NOT NULL,
 	CONSTRAINT tournaments_pk PRIMARY KEY (id_tournament),
-	CONSTRAINT tournaments_FK FOREIGN KEY (id_admin) REFERENCES proyectoGaming.admins(id_admin) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT tournaments_FK_1 FOREIGN KEY (id_game) REFERENCES proyectoGaming.games(id_game) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT tournaments_FK FOREIGN KEY (id_admin) REFERENCES admins(id_admin) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT tournaments_FK_1 FOREIGN KEY (id_game) REFERENCES games(id_game) ON DELETE CASCADE ON UPDATE CASCADE
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci;
-DROP TABLE IF EXISTS proyectoGaming.tournaments;
 
-CREATE TABLE proyectoGaming.participants (
+
+CREATE TABLE participants (
 	id_participant INT auto_increment NOT NULL,
 	id_tournament INT NOT NULL,
 	id_seat INT NOT NULL,
 	id_user INT NOT NULL,
 	CONSTRAINT participants_pk PRIMARY KEY (id_participant),
-	CONSTRAINT participants_FK FOREIGN KEY (id_user) REFERENCES proyectoGaming.users(id_user) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT participants_FK_1 FOREIGN KEY (id_seat) REFERENCES proyectoGaming.seats(id_seat) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT participants_FK_2 FOREIGN KEY (id_tournament) REFERENCES proyectoGaming.tournaments(id_tournament) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT participants_FK FOREIGN KEY (id_user) REFERENCES users(id_user) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT participants_FK_1 FOREIGN KEY (id_seat) REFERENCES seats(id_seat) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT participants_FK_2 FOREIGN KEY (id_tournament) REFERENCES tournaments(id_tournament) ON DELETE CASCADE ON UPDATE CASCADE
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci;
-DROP TABLE IF EXISTS proyectoGaming.participants;
 
-CREATE TABLE proyectoGaming.wins (
+
+CREATE TABLE wins (
 	id_win INT auto_increment NOT NULL,
 	`number` INT NULL,
 	id_admin INT NOT NULL,
 	id_participant INT NOT NULL,
 	CONSTRAINT wins_pk PRIMARY KEY (id_win),
-	CONSTRAINT wins_FK FOREIGN KEY (id_admin) REFERENCES proyectoGaming.admins(id_admin) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT wins_FK_1 FOREIGN KEY (id_participant) REFERENCES proyectoGaming.participants(id_participant) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT wins_FK FOREIGN KEY (id_admin) REFERENCES admins(id_admin) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT wins_FK_1 FOREIGN KEY (id_participant) REFERENCES participants(id_participant) ON DELETE CASCADE ON UPDATE CASCADE
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci;
-DROP TABLE IF EXISTS proyectoGaming.wins;
+
 
