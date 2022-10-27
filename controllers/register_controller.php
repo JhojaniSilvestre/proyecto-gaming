0ec880b17710107e3a4fb3c54feb4bba8b7b8095
edu zafra <eduardozaframartin@gmail.com>
2022-10-27 16:53:54 +0200

    registro

--- controllers/register_controller.php
+++ controllers/register_controller.php
@@ -1,22 +1,24 @@
 <?php
-//require_once ('../models/register_model.php');
+require_once('../models/register_model.php');
 
 //si el formulario se ha enviado
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-    $name=$_POST['name'];
-    $email=$_POST['email'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
     $password = $_POST['password'];
+    $shift = $_POST['shift'];
 
-    //Aquí se validad si los campos están vacios, falta validar nombre y email para que la validación sea completa.
-    
-        if (validateUser ($idUser,$name,$email,$password)){
-            emptyField ($idUser,$name,$email,$password);
 
-            echo "Registro Completado";
-        }else{
-    
-            echo " NO se ha registrado correctamente";
-        }
+    //Aquí se validad si los campos están vacios, falta validar nombre y email para que la validación sea completa.0
+
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        registerUser($email, $password, $shift, $name);
+        header("location: ../views/login_view.php");
+    } else {
+        $email_err = "Email incorrecto";
+    }
 }
-    require_once("../views/register_view.php");
+
+require_once("../views/register_view.php");
