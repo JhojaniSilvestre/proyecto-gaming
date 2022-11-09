bd5a99afd3b6d359b48ec974f6c7f235e69d981e
JhojaniSilvestre <maellysilvestre@gmail.com>
2022-11-09 22:23:40 +0100

    pagina torneo, crear y editar - insertar torneo

--- views/crud_tournament/create_view.php
+++ views/crud_tournament/create_view.php
@@ -5,12 +5,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Panel Admin</title>
+    <title>Crear torneo</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../css/bootstrap.min.css" rel="stylesheet">
     <!-- Estilo customizado -->
-    <link href="../css/dashboard.css" rel="stylesheet">
+    <link href="../../css/dashboard.css" rel="stylesheet">
+    <!--Trae el estilo del formulario-->
+    <link href="../../css/booking_style.css" rel="stylesheet">
     <!-- Font Awesome icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
@@ -18,7 +20,7 @@
 <body>
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
-            <img src="../img/gamin-room-logo-purple.png" alt="gaming room logo" class="img-logo-admin">
+            <img src="../../img/gamin-room-logo-purple.png" alt="gaming room logo" class="img-logo-admin">
         </a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -42,8 +44,8 @@
             <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse">
                 <div class="position-sticky pt-5">
                     <ul class="nav flex-column">
-                        <li class="nav-item  menu-items active">
-                            <a class="nav-link" aria-current="page" href="./admin_controller.php">
+                        <li class="nav-item  menu-items">
+                            <a class="nav-link" aria-current="page" href="../../controllers/admin_controller.php">
                                 <span class="fa-stack fa-1x">
                                     <i class="fa-solid fa-circle fa-stack-2x"></i>
                                     <i class="fa-solid fa-gauge-simple-high fa-stack-1x fa-inverse"></i>
@@ -60,8 +62,8 @@
                                 Usuarios
                             </a>
                         </li>
-                        <li class="nav-item menu-items">
-                            <a class="nav-link" aria-current="page" href="./tournament_controller.php">
+                        <li class="nav-item menu-items active">
+                            <a class="nav-link" aria-current="page" href="../../controllers/tournament_controller.php">
                                 <span class="fa-stack fa-1x">
                                     <i class="fa-solid fa-circle fa-stack-2x"></i>
                                     <i class="fa-solid fa-laptop fa-stack-1x fa-inverse"></i>
@@ -89,7 +91,7 @@
                         </li>
                         <hr class="bg-danger border-2 border-top mx-2">
                         <li class="nav-item menu-items">
-                            <a class="nav-link" aria-current="page" href="../index.php">
+                            <a class="nav-link" aria-current="page" href="../../index.php">
                                 <span class="fa-stack fa-1x">
                                     <i class="fa-solid fa-circle fa-stack-2x"></i>
                                     <i class="fa-solid fa-right-from-bracket fa-stack-1x fa-inverse"></i>
@@ -103,70 +105,50 @@
             <!----------------------------------------START MAIN SECTION ------------------------------------------>
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
-                    <h1 class="text-white fw-bold h3">Dashboard</h1>
+                    <h1 class="text-white fw-bold h3">Crear Torneos</h1>
                 </div>
 
                 <div class="row">
-                    <div class="col-md-4 order-2 order-md-1 mt-4 mt-md-0 d-flex align-items-stretch">
-                        <div class="card">
-                            <img src="../img/banner-bg.jpg" class="card-img-top rounded p-4" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Panel Admin</h5>
-                                <p class="card-text">En este panel podrás crear y gestionar todos los torneos, juegos, victorias y usuarios de la web.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-8 order-1 order-md-2">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="d-flex flex-row justify-content-between m-3">
-                                    <h4 class="card-title mb-1">Administrar</h4>
-                                    <p class="text-muted mb-1">Acciones Rápidas</p>
+                    <div class="card col-lg-5 col-10 mx-auto">
+                        <div class="card-body px-2 py-3 px-md-3 py-md-5">
+                            <h3 class="card-title text-left mb-3">Crear nuevo torneo</h3>
+                            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                                <div>
+                                    <input type="text" name="nombre" id="nombre" placeholder="Nombre del torneo">
                                 </div>
-                                <div class="row">
-                                    <div class="col-12">
-                                        <div class="preview-item d-flex justify-content-between m-3">
-                                            <div class="preview-thumbnail pb-2">
-                                                <span class="fa-stack fa-2x">
-                                                    <i class="fa-solid fa-square fa-stack-2x text-primary"></i>
-                                                    <i class="fa-solid fa-laptop fa-stack-1x fa-inverse"></i>
-                                                </span>
-                                                <span class="preview-text fs-5 fw-bold">Torneos</span>
-                                            </div>
-                                            <div class="preview-item-content d-flex align-items-center">
-                                                <input type="button" value="Crear" onclick="window.location.href='../controllers/crud_tournament/create_controller.php'" 
-                                                class="btn btn-outline-info">
-                                            </div>
-                                        </div>
-                                        <div class="preview-item d-flex justify-content-between m-3">
-                                            <div class="preview-thumbnail pb-2">
-                                                <span class="fa-stack fa-2x">
-                                                    <i class="fa-solid fa-square fa-stack-2x text-success"></i>
-                                                    <i class="fa-solid fa-gamepad fa-stack-1x fa-inverse"></i>
-                                                </span>
-                                                <span class="preview-text fs-5 fw-bold">Juegos</span>
-                                            </div>
-                                            <div class="preview-item-content d-flex align-items-center">
-                                                <input type="button" value="Añadir" onclick="window.location.href='#'" 
-                                                class="btn btn-outline-success">
-                                            </div>
-                                        </div>
-                                        <div class="preview-item d-flex justify-content-between m-3">
-                                            <div class="preview-thumbnail pb-2">
-                                                <span class="fa-stack fa-2x">
-                                                    <i class="fa-solid fa-square fa-stack-2x text-danger"></i>
-                                                    <i class="fa-solid fa-trophy fa-stack-1x fa-inverse"></i>
-                                                </span>
-                                                <span class="preview-text fs-5 fw-bold">Victorias</span>
-                                            </div>
-                                            <div class="preview-item-content d-flex align-items-center">
-                                                <input type="button" value="Añadir" onclick="window.location.href='#'" 
-                                                class="btn btn-outline-danger">
-                                            </div>
-                                        </div>
-                                    </div>
+                                <div>
+                                    <input type="date" name="fecha" id="fecha" placeholder="Fecha del torneo">
                                 </div>
-                            </div>
+                                <div>
+                                    <select name="juego" required>
+                                        <!--recorro el array  -->
+                                        <?php foreach($games as $id_game => $game) : ?>
+                                            <?php echo "<option value='".$id_game."'>".$game."</option>"; ?>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+                                <div class="mt-3 mb-4">
+                                    <label class="mb-3 me-2" for="shift">Hora:</label>
+
+                                    <input type="radio" class="btn-check" name="turno" id="m" value="m" autocomplete="off" required>
+                                    <label class="btn btn-sm btn-outline-secondary" for="m">11:15 - 11:40</label>
+
+                                    <input type="radio" class="btn-check" name="turno" id="t" value="t" autocomplete="off" required>
+                                    <label class="btn btn-sm btn-outline-secondary" for="t">17:45 - 18:15</label>
+                                </div>
+                                <div class="text-center mb-2">
+                                    <button type="submit" name="crear" class="btn-form">Crear</button>
+                                </div>
+                                <!-- Imprimo msj error-->
+                                <?php if(empty($errors) === false){ ?>
+                                    <?php echo "<ul>"; ?>
+                                        <?php foreach($errors as $error) : ?>
+                                            <?php echo "<li class='text-danger'>".$error."</li>"; ?>
+                                        <?php endforeach; ?>
+                                    <?php echo "</ul>"; ?>
+                                <?php } ?>
+                                <!-- fin msj error -->
+                            </form>
                         </div>
                     </div>
                 </div>
@@ -175,7 +157,7 @@
         </div>
     </div>
     <!---------------------------------- Scripts ---------------------------->
-    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../../js/bootstrap.bundle.min.js"></script>
 
 </body>
 
--- views/crud_tournament/edit_view.php
+++ views/crud_tournament/edit_view.php
@@ -1,3 +1,12 @@
+<?php
+    session_start();
+    
+    if (!isset($_SESSION['id_admin'])) {
+		session_unset();
+		session_destroy();
+		header("location: ../../index.php");
+	}
+?>
 <!doctype html>
 <html lang="es">
 
@@ -8,9 +17,11 @@
     <title>Panel Admin</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../css/bootstrap.min.css" rel="stylesheet">
     <!-- Estilo customizado -->
-    <link href="../css/dashboard.css" rel="stylesheet">
+    <link href="../../css/dashboard.css" rel="stylesheet">
+    <!--Trae el estilo del formulario-->
+    <link href="../../css/booking_style.css" rel="stylesheet">
     <!-- Font Awesome icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
@@ -18,7 +29,7 @@
 <body>
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
-            <img src="../img/gamin-room-logo-purple.png" alt="gaming room logo" class="img-logo-admin">
+            <img src="../../img/gamin-room-logo-purple.png" alt="gaming room logo" class="img-logo-admin">
         </a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -42,8 +53,8 @@
             <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse">
                 <div class="position-sticky pt-5">
                     <ul class="nav flex-column">
-                        <li class="nav-item  menu-items active">
-                            <a class="nav-link" aria-current="page" href="./admin_controller.php">
+                        <li class="nav-item  menu-items">
+                            <a class="nav-link" aria-current="page" href="../../controllers/admin_controller.php">
                                 <span class="fa-stack fa-1x">
                                     <i class="fa-solid fa-circle fa-stack-2x"></i>
                                     <i class="fa-solid fa-gauge-simple-high fa-stack-1x fa-inverse"></i>
@@ -60,8 +71,8 @@
                                 Usuarios
                             </a>
                         </li>
-                        <li class="nav-item menu-items">
-                            <a class="nav-link" aria-current="page" href="./tournament_controller.php">
+                        <li class="nav-item menu-items active">
+                            <a class="nav-link" aria-current="page" href="../../controllers/tournament_controller.php">
                                 <span class="fa-stack fa-1x">
                                     <i class="fa-solid fa-circle fa-stack-2x"></i>
                                     <i class="fa-solid fa-laptop fa-stack-1x fa-inverse"></i>
@@ -89,7 +100,7 @@
                         </li>
                         <hr class="bg-danger border-2 border-top mx-2">
                         <li class="nav-item menu-items">
-                            <a class="nav-link" aria-current="page" href="../index.php">
+                            <a class="nav-link" aria-current="page" href="../../index.php">
                                 <span class="fa-stack fa-1x">
                                     <i class="fa-solid fa-circle fa-stack-2x"></i>
                                     <i class="fa-solid fa-right-from-bracket fa-stack-1x fa-inverse"></i>
@@ -103,79 +114,52 @@
             <!----------------------------------------START MAIN SECTION ------------------------------------------>
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
-                    <h1 class="text-white fw-bold h3">Dashboard</h1>
+                    <h1 class="text-white fw-bold h3">Editar Torneo</h1>
                 </div>
 
                 <div class="row">
-                    <div class="col-md-4 order-2 order-md-1 mt-4 mt-md-0 d-flex align-items-stretch">
-                        <div class="card">
-                            <img src="../img/banner-bg.jpg" class="card-img-top rounded p-4" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Panel Admin</h5>
-                                <p class="card-text">En este panel podrás crear y gestionar todos los torneos, juegos, victorias y usuarios de la web.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-8 order-1 order-md-2">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="d-flex flex-row justify-content-between m-3">
-                                    <h4 class="card-title mb-1">Administrar</h4>
-                                    <p class="text-muted mb-1">Acciones Rápidas</p>
+                    <div class="card col-lg-5 col-10 mx-auto">
+                        <div class="card-body px-2 py-3 px-md-3 py-md-5">
+                            <h3 class="card-title text-left mb-3">Editar información del torneo</h3>
+                            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                                <div>
+                                    <input type="text" name="cnombre" id="cnombre" placeholder="Nombre del torneo">
                                 </div>
-                                <div class="row">
-                                    <div class="col-12">
-                                        <div class="preview-item d-flex justify-content-between m-3">
-                                            <div class="preview-thumbnail pb-2">
-                                                <span class="fa-stack fa-2x">
-                                                    <i class="fa-solid fa-square fa-stack-2x text-primary"></i>
-                                                    <i class="fa-solid fa-laptop fa-stack-1x fa-inverse"></i>
-                                                </span>
-                                                <span class="preview-text fs-5 fw-bold">Torneos</span>
-                                            </div>
-                                            <div class="preview-item-content d-flex align-items-center">
-                                                <input type="button" value="Crear" onclick="window.location.href='../controllers/crud_tournament/create_controller.php'" 
-                                                class="btn btn-outline-info">
-                                            </div>
-                                        </div>
-                                        <div class="preview-item d-flex justify-content-between m-3">
-                                            <div class="preview-thumbnail pb-2">
-                                                <span class="fa-stack fa-2x">
-                                                    <i class="fa-solid fa-square fa-stack-2x text-success"></i>
-                                                    <i class="fa-solid fa-gamepad fa-stack-1x fa-inverse"></i>
-                                                </span>
-                                                <span class="preview-text fs-5 fw-bold">Juegos</span>
-                                            </div>
-                                            <div class="preview-item-content d-flex align-items-center">
-                                                <input type="button" value="Añadir" onclick="window.location.href='#'" 
-                                                class="btn btn-outline-success">
-                                            </div>
-                                        </div>
-                                        <div class="preview-item d-flex justify-content-between m-3">
-                                            <div class="preview-thumbnail pb-2">
-                                                <span class="fa-stack fa-2x">
-                                                    <i class="fa-solid fa-square fa-stack-2x text-danger"></i>
-                                                    <i class="fa-solid fa-trophy fa-stack-1x fa-inverse"></i>
-                                                </span>
-                                                <span class="preview-text fs-5 fw-bold">Victorias</span>
-                                            </div>
-                                            <div class="preview-item-content d-flex align-items-center">
-                                                <input type="button" value="Añadir" onclick="window.location.href='#'" 
-                                                class="btn btn-outline-danger">
-                                            </div>
-                                        </div>
-                                    </div>
+                                <div>
+                                    <input type="date" name="cfecha" id="cfecha" placeholder="Fecha del torneo">
                                 </div>
-                            </div>
+                                <div>
+                                    <select required>
+                                        <option selected disabled value="">Juego</option>
+                                        <!--recorro el array  -->
+                                        <?php foreach ($seats as $seat_id) : ?>
+                                            <?php echo "<option>" . $seat_id . "</option>"; ?>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+                                <div class="mt-3 mb-4">
+                                    <label class="mb-3 me-2" for="shift">Hora:</label>
+
+                                    <input type="radio" class="btn-check" name="shift" id="m" value="m" autocomplete="off" required>
+                                    <label class="btn btn-sm btn-outline-secondary" for="m">11:15 - 11:40</label>
+
+                                    <input type="radio" class="btn-check" name="shift" id="t" value="t" autocomplete="off" required>
+                                    <label class="btn btn-sm btn-outline-secondary" for="t">17:45 - 18:15</label>
+                                </div>
+                                <div class="text-center">
+                                    <button type="submit" class="btn-form">Actualizar</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
+                
             </main>
             <!----------------------------------------END MAIN SECTION ------------------------------------------>
         </div>
     </div>
     <!---------------------------------- Scripts ---------------------------->
-    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../../js/bootstrap.bundle.min.js"></script>
 
 </body>
 
