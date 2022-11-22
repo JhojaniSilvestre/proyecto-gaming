025a802e3e6375753763afb8c28099295b2973c2
edu zafra <eduardozaframartin@gmail.com>
2022-11-22 20:43:37 +0100

    reservas,registro errores

--- controllers/booking_controller.php
+++ controllers/booking_controller.php
@@ -1,20 +1,82 @@
 <?php
-    session_start();
-    
-    if (!isset($_SESSION['id_user'])) {
-		session_unset();
-		session_destroy();
-		header("location: ../index.php");
+session_start();
+
+if (!isset($_SESSION['id_user'])) {
+	session_unset();
+	session_destroy();
+	header("location: ../index.php");
+}
+
+require_once '../db/db.php';
+$conn = generarConexion();
+
+require_once '../models/booking_model.php';
+//array que contiene las pelis disponibles
+$seats = obtenerPuestos($conn);
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+	if (isset($_POST['submit'])) {
+		$errors = array();
+		$correct = true;
+		if ($_POST["emailUser"] == "" || $_POST["fecha"] == "" || $_POST["seat"] == "" || $_POST["shift"] == "") {
+			array_push($errors, "No puede dejar campos en blanco o sin seleccionar.");
+			$correct = false;
+		} else {
+			$emailUser = limpiar($_POST["emailUser"]);
+			$emailAcomp = limpiar($_POST["emailAcomp"]);
+			$seat = limpiar($_POST["seat"]);
+			$date = limpiar($_POST["fecha"]);
+			$turn = limpiar($_POST["shift"]);
+			$idComp = 'NULL';
+
+			//fecha formato datetime según la hora elegida
+			if ($turn == "m")
+				$datetime = $date . " 11:15:00";
+			else
+				$datetime = $date . " 17:45:00";
+
+			$idUsuario = comprobarEmail($conn, $emailUser);
+			if ($idUsuario == "") {
+				array_push($errors, "El email es incorrecto");
+				$correct = false;
+			}
+
+			if ($emailAcomp != "") {
+				$idComp = comprobarEmail($conn, $emailAcomp);
+				if ($idComp == "") {
+					array_push($errors, "El email del acompañante es incorrecto");
+					$correct = false;
+				}
+			}
+
+			$resultado = comprobarSitioLibre($conn, $seat, $datetime);
+			if ($resultado == false) {
+				array_push($errors, "Sitio no disponible");
+				$correct = false;
+			}
+
+			/* funcion para obtener la fecha actual */
+			date_default_timezone_set('Europe/Madrid');
+			$dateNow = date("Y-m-d");
+			if ($_SESSION['shift'] != $turn) {
+				array_push($errors, "No puede reservar un turno al que no pertenezca");
+				$correct = false;
+			}
+			if ($date < $dateNow) {
+				array_push($errors, "Introduzca una fecha correcta");
+				$correct = false;
+			}
+		}
+
+		if ($correct) {
+			reservaPuesto($conn, $datetime, $seat, $idUsuario, $idComp);
+			
+		}
 	}
 
-	require_once '../db/db.php';
-	$conn = generarConexion();
-
-	require_once '../models/booking_model.php';
-	//array que contiene las pelis disponibles
-	$seats = obtenerPuestos($conn);
-
 	cerrarConexion($conn);
+}
 
-	require_once("../views/booking_view.php");
-?>
+
+cerrarConexion($conn);
+
+require_once("../views/booking_view.php");
--- models/booking_model.php
+++ models/booking_model.php
@@ -1,23 +1,64 @@
 <?php
-function obtenerPuestos($conn){
+function obtenerPuestos($conn)
+{
     try {
         $stmt = $conn->prepare("SELECT id_seat FROM seats");
         $stmt->execute(); //ejecuta la select
 
         $seats = array();
-        
-        if($stmt->rowCount() > 0){
+
+        if ($stmt->rowCount() > 0) {
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
-            foreach($stmt->fetchAll() as $row) {
-                array_push($seats,$row["id_seat"]);
+            foreach ($stmt->fetchAll() as $row) {
+                array_push($seats, $row["id_seat"]);
             }
         }
         return $seats; //devuelvo el array
+
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+function comprobarEmail($conn, $emailUser)
+{
+    try {
+        $stmt = $conn->prepare("SELECT id_user FROM users where email = '$emailUser' AND active = 1 ");
+        $stmt->execute(); //ejecuta la select
+        $idUser="";
+        if ($stmt->rowCount() > 0) {
+            $idUser = $stmt->fetchColumn();
+        }
+        return $idUser;
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+function comprobarSitioLibre($conn,$seat,$date){
+    try {
+        $stmt = $conn->prepare("SELECT seats.id_seat FROM seats,booking where seats.id_seat=booking.id_seat 
+        AND date = '$date' AND booking.id_seat=$seat");
+        $stmt->execute(); //ejecuta la select
+        $correcto=true;
+        if ($stmt->rowCount() > 0) {
+            $correcto= false;
+        }
+        return $correcto;
         
     }
     catch(PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
-}
+} 
 
-?>
+function reservaPuesto($conn,$date,$idSeat,$idUser,$idComp){
+    try {
+        $insert = "INSERT INTO booking (date,id_seat,id_user,id_companion) 
+        VALUES ('$date',$idSeat,$idUser,$idComp)";
+        $conn->exec($insert);
+
+	}catch(PDOException $e){
+		 echo "Error: ", $e-> getMessage();
+	}
+}
