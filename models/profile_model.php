7e7c944576222084f3a86c4f01f0efda1235b7ac
EduardoZafraMartin <72192046+EduardoZafraMartin@users.noreply.github.com>
2022-12-03 13:38:14 +0100

    Update profile_model.php

--- models/profile_model.php
+++ models/profile_model.php
@@ -24,9 +24,9 @@ function obtenerMisReservas($conn,$id_user)
 function obtenerMisTorneos($conn,$id_user)
 {
     try {
-        $stmt = $conn->prepare("SELECT tournaments.id_tournament,tournaments.name AS nametourn,date, games.name AS nomgame,username, participants.id_seat
-         FROM tournaments,games,users,participants WHERE tournaments.id_game = games.id_game AND users.id_user = tournaments.responsible
-          AND tournaments.id_tournament = participants.id_tournament AND participants.id_user = $id_user  AND tournaments.active = 1; ");
+        $stmt = $conn->prepare("SELECT tournaments.id_tournament,tournaments.name AS nametourn,date, games.name AS nomgame,username, participants.id_seat,participants.id_user 
+        FROM tournaments,games,users,participants WHERE tournaments.id_game = games.id_game AND tournaments.id_tournament = participants.id_tournament 
+        AND participants.id_user = $id_user AND tournaments.active = 1 ");
         $stmt->execute();
         $tournaments = array();
 
