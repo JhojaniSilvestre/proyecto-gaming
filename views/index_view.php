26c89632fb99b1d089435f590a93a9fb3098adea
JhojaniSilvestre <maellysilvestre@gmail.com>
2022-12-12 14:06:50 +0100

    inicio responsive arreglado

--- views/index_view.php
+++ views/index_view.php
@@ -6,35 +6,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gaming</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Additional CSS Files -->
     <link rel="stylesheet" href="css/index_style.css">
 </head>
 
 <body>
     <div class="container">
-        <div class="card">
-            <div class="box">
-                <div class="content">
-                    <h2>Leonardo</h2>
-                    <h3>Login Admin</h3>
-                    <a href="controllers/adminLogin_controller.php">Tengo una cuenta</a>
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card mx-auto">
+                    <div class="box">
+                        <div class="content">
+                            <h2>Leonardo</h2>
+                            <h3>Login Admin</h3>
+                            <a href="controllers/adminLogin_controller.php">Tengo una cuenta</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="card">
-            <div class="box">
-                <div class="content">
-                    <h2>IES</h2>
-                    <h3>Login Usuario</h3>
-                    <a href="controllers/userLogin_controller.php">Tengo una cuenta</a>
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card mx-auto">
+                    <div class="box">
+                        <div class="content">
+                            <h2>IES</h2>
+                            <h3>Login Usuario</h3>
+                            <a href="controllers/userLogin_controller.php">Tengo una cuenta</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="card">
-            <div class="box">
-                <div class="content">
-                    <h2>Da Vinci</h2>
-                    <h3>Registrate</h3>
-                    <a href="controllers/register_controller.php">Registrate YA</a>
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card mx-auto">
+                    <div class="box">
+                        <div class="content">
+                            <h2>Da Vinci</h2>
+                            <h3>Registrate</h3>
+                            <a href="controllers/register_controller.php">Registrate YA</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
